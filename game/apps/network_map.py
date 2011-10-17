dedc7af7b08e6cd58f41e3714ede55ca4c0331ca
Teifion <sarkalian@gmail.com>
2011-10-17 23:44:53 +0100

    Added basics for apps

---
+++ game/apps/network_map.py
@@ -0,0 +1,15 @@
+import pygame
+
+from game import application
+
+class NetworkMap (application.Application):
+    def __init__(self, size, position, network, priority=0):
+        super(NetworkMap, self).__init__(size, position, priority)
+        
+        self.network = network
+    
+    def draw(self):
+        self._image = pygame.Surface(self.rect.size)
+        self._image.fill((255, 255, 255), pygame.Rect(0, 0, self.rect.width, self.rect.height))
+    
+
