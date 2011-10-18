9e6df63c1210514061341e38c37d122b4abb448a
Teifion <sarkalian@gmail.com>
2011-10-18 23:05:27 +0100

    Corrected mis-ordering of size and position in init arguments

--- game/apps/network_map.py
+++ game/apps/network_map.py
@@ -4,12 +4,14 @@ from game import application
 
 class NetworkMap (application.Application):
     def __init__(self, size, position, network, priority=0):
-        super(NetworkMap, self).__init__(size, position, priority)
+        super(NetworkMap, self).__init__(position, size, priority)
         
         self.network = network
     
     def draw(self):
         self._image = pygame.Surface(self.rect.size)
         self._image.fill((255, 255, 255), pygame.Rect(0, 0, self.rect.width, self.rect.height))
+        
+        print(self.rect)
     
 
