e7305d89b021934f8cf00efc4ab5295202b71e99
Teifion <sarkalian@gmail.com>
2011-10-20 14:43:38 +0100

    Deleted file

--- game/application.py
+++
@@ -1,9 +0,0 @@
-from engine.render import controls
-
-class Application (controls.Panel):
-    def __init__(self, position, size, priority=0):
-        super(Application, self).__init__(position, size, priority)
-    
-    def logic_cycle(self):
-        pass
-
