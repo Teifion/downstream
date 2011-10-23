b810b49d00992cd4c374eb7a9a0fecbc637be70f
Teifion <sarkalian@gmail.com>
2011-10-23 14:08:21 +0100

    Made useful

--- game/apps/cracker.py
+++ game/apps/cracker.py
@@ -1,20 +1,56 @@
-from game.classes import application, job
+from game.classes import application, job, user
+
+base_time = 1000
+
+crack_times = {
+    "brute_force":  1,
+    "dictionary":   0.5,
+    "rainbow":      0.1,
+}
 
 class Cracker (application.Application):
     def __init__(self, **app_data):
         super(Cracker, self).__init__(app_type="cracker", **app_data)
     
-    def _cpu_cost(self, version):
-        pass
+    def _cracking_power(self, version):
+        return {
+            "brute_force":  version + 1,
+            "dictionary":   version + 1,
+            "rainbow":      version + 1,
+        }
     
-    def launch(self, owner, version):
+    def _max_progress(self, version, target_password, cracking_power):
+        crack_time = 99999999
+        for k, v in target_password.items():
+            if k == "id": continue
+            
+            ratio = user.crack_ratio(v, cracking_power.get(k, 0))
+            c_time = ratio * base_time * crack_times[k]
+            
+            # If it's zero then it's uncrackable via that method
+            if c_time > 0:
+                crack_time = min(crack_time, c_time)
+        
+        return crack_time
+    
+    def launch(self, owner, version, target_password):
         j = job.Job(
-            owner       = owner,
-            version     = version,
-            short_name  = self.data['short_name'],
-            full_name   = self.data['full_name'],
+            owner           = owner,
+            version         = version,
+            short_name      = self.data['short_name'],
+            full_name       = self.data['full_name'],
+            
+            max_progress    = self._max_progress(version, target_password, self._cracking_power(version))
         )
         
-        # Override the job's _cycle function
+        def _cycle(self, cpu_points=1):
+            pass
         
+        def _complete():
+            pass
+        
+        j._cycle    = _cycle
+        j._complete = _complete
+        
+        return j
 
