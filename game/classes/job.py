37a735d3be7dfacdbefc7bd9f8f543e46a55598d
Teifion <sarkalian@gmail.com>
2011-10-23 14:52:35 +0100

    Added several properties

--- game/classes/job.py
+++ game/classes/job.py
@@ -5,22 +5,26 @@ class Job (object):
     there's no future mixup with the Process from the Multiprocessing
     module."""
     
-    def __init__(self, owner, version=1, short_name="jbclas", full_name="Job class", max_progress=1, max_cpu=-1):
+    def __init__(self, owner, version=1, short_name="jbclas", full_name="Job class", max_progress=1, max_cpu=1):
         super(Job, self).__init__()
         
         self.progress = 0
         self.max_progress = max_progress
         
-        self.owner = owner
+        self.owner  = owner
+        self.node   = -1
         
         self.short_name = short_name
         self.full_name  = full_name
+        self.version    = float(version)
         
         # It's possible that an app cannot run beyond a certain speed
         self.max_cpu = max_cpu
+        
+        self.is_complete = False
     
     def get_progress(self):
-        return (self.max_progress / self.progress) * 100
+        return (self.progress/self.max_progress) * 100
     
     def _cycle(self, cpu_points = 1):
         raise Exception("Not implemented")
@@ -29,11 +33,14 @@ class Job (object):
         raise Exception("Not implemented")
     
     def cycle(self, cpu_points = 1):
+        if self.is_complete: return
+        
         self._cycle(cpu_points)
         self.progress += min(cpu_points, self.max_cpu)
         
         if self.progress >= self.max_progress:
             self._complete()
+            self.is_complete = True
 
 
 
