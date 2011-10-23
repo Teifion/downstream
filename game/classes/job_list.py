c5f188bc8f8ac80580dad63665987e3af2c8be93
Teifion <sarkalian@gmail.com>
2011-10-23 14:52:23 +0100

    Added code to draw the job list/table and also code to handle mouse ups (though the actionable part of the code is not done yet)

--- game/classes/job_list.py
+++ game/classes/job_list.py
@@ -1,6 +1,14 @@
+from __future__ import division
+
+import math
+
 import pygame
 
 from engine.render import controls
+
+row_height = 22
+header_size = 19
+row_size = 17
 
 class JobList (controls.Panel):
     always_redraw = True
@@ -9,17 +17,64 @@ class JobList (controls.Panel):
         super(JobList, self).__init__(position, size, priority)
         
         self.network = network
+        
+        self.fill_colour = fill_colour
+        self.text_colour = text_colour
     
     def draw(self):
         self._image = pygame.Surface(self.rect.size)
-        self._image.fill((0, 0, 0), pygame.Rect(0, 0, self.rect.width, self.rect.height))
+        self._image.fill(self.fill_colour, pygame.Rect(0, 0, self.rect.width, self.rect.height))
         
         if self.network == None:
             return
         
-        i = -1
-        for job_id, the_job in self.network.jobs.items():
+        controls.draw_text(self._image, "Job", (5, 5), self.text_colour, size=header_size, bold=True)
+        controls.draw_text(self._image, "Node", (70, 5), self.text_colour, size=header_size, bold=True)
+        controls.draw_text(self._image, "App", (130, 5), self.text_colour, size=header_size, bold=True)
+        controls.draw_text(self._image, "%", (215, 5), self.text_colour, size=header_size, bold=True)
+        
+        key_list = list(self.network.jobs.keys())
+        key_list.sort()
+        
+        i = 0
+        for job_id in key_list:
+            the_job = self.network.jobs[job_id]
             i += 1
             
-            controls.draw_text(self._image, str(job_id), (5, 5+i*20), (255, 255, 255))
+            # Job ID
+            controls.draw_text(self._image, str(job_id),
+                (5, 5+i*row_height), self.text_colour, size=row_size)
+            
+            # Node ID
+            controls.draw_text(self._image, str(the_job.node),
+                (70, 5+i*row_height), self.text_colour, size=row_size)
+            
+            # App name
+            controls.draw_text(self._image, "%s%s" % (the_job.short_name, the_job.version),
+                (130, 5+i*row_height), self.text_colour, size=row_size)
+            
+            # Progress
+            controls.draw_text(self._image, "%s%%" % int(the_job.get_progress()),
+                (215, 5+i*row_height), self.text_colour, size=row_size)
+
+    def handle_mouseup(self, event):
+        relative_pos = (
+            event.pos[0] - self.rect.left,
+            event.pos[1] - self.rect.top,
+        )
+        
+        row = int(math.floor(relative_pos[1] / row_height)) - 1
+        
+        # Clicked header
+        if row == -1:
+            return
+        
+        key_list = list(self.network.jobs.keys())
+        key_list.sort()
+        
+        # Clicked too far down
+        if row > len(key_list):
+            return
+        
+        print("Need to show app window for %s" % key_list[row])
     
