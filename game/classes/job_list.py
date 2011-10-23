b45ea3626c2caf5aae16d3a94282997f6f56a22d
Teifion <sarkalian@gmail.com>
2011-10-23 14:09:01 +0100

    So far it just draws the job ID of any jobs running

--- game/classes/job_list.py
+++ game/classes/job_list.py
@@ -5,19 +5,21 @@ from engine.render import controls
 class JobList (controls.Panel):
     always_redraw = True
     
-    def __init__(self, job_dict, size, position, priority=0, fill_colour=(0,0,0), text_colour=(255, 255, 255)):
+    def __init__(self, network, size, position, priority=0, fill_colour=(0,0,0), text_colour=(255, 255, 255)):
         super(JobList, self).__init__(position, size, priority)
         
-        self.jobs = job_dict
+        self.network = network
     
     def draw(self):
         self._image = pygame.Surface(self.rect.size)
         self._image.fill((0, 0, 0), pygame.Rect(0, 0, self.rect.width, self.rect.height))
         
+        if self.network == None:
+            return
+        
         i = -1
-        for job_id, the_job in self.jobs.items():
+        for job_id, the_job in self.network.jobs.items():
             i += 1
-            print(dir(the_job))
             
-            controls.draw_text(self._image, job_id, (5, 5+i*20), (255, 255, 255))
+            controls.draw_text(self._image, str(job_id), (5, 5+i*20), (255, 255, 255))
     
